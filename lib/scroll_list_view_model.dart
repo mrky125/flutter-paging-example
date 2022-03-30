071c4605d8cc09bfcf59098f48f2d5853ebf4d48
mrky125 <4ex21ia@gmail.com>
2022-03-30 17:53:49 +0900

    update view model

--- lib/scroll_list_view_model.dart
+++ lib/scroll_list_view_model.dart
@@ -1,27 +1,79 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class ScrollListViewModel extends StateNotifier<List<String>> {
-  ScrollListViewModel() : super([]) {
-    fetchList();
+import 'data/page_state.dart';
+import 'data/post_list_state.dart';
+
+final postListControllerProvider =
+    StateNotifierProvider.autoDispose<PostListController, PostListState>(
+        (ref) => PostListController(ref.read));
+
+class PostListController extends StateNotifier<PostListState> {
+  PostListController(this._read) : super(const PostListState()) {
+    fetch();
   }
 
-  // once page size
-  static const _addCount = 10;
+  @visibleForTesting
+  PostListController.withDefaultValue(
+    PostListState state,
+    this._read,
+  ) : super(state);
 
-  Future<void> fetchList() async {
-    // dummy fetch
-    final newList = await fetchNextListByDummyRepository();
-    // set data
-    state = state + newList;
+  final Reader _read;
+
+  // PostRepository get _repo => _read(postRepositoryProvider);
+
+  static const perPage = 10;
+
+  @visibleForTesting
+  Future<void> fetch({
+    bool loadMore = false,
+  }) async {
+    state = state.copyWith(pageState: const PageStateLoading());
+
+    // final newItems = await _repo.fetch(
+    //   page: state.page,
+    //   perPage: perPage,
+    //   query: state.query,
+    // );
+    final newItems = await fetchNextListByDummyRepository();
+    state = state.copyWith(
+      posts: [if (loadMore) ...state.posts, ...newItems],
+      hasNext: newItems.length >= perPage,
+      pageState: const PageStateSuccess(),
+    );
   }
 
   Future<List<String>> fetchNextListByDummyRepository() async =>
       Future.delayed(const Duration(seconds: 2), () {
         final List<String> items = [];
-        for (var i = 0; i < _addCount; i++) {
-          final id = state.length + i + 1;
+        for (var i = 0; i < perPage; i++) {
+          final id = state.posts.length + i + 1;
           items.add('item $id');
         }
         return items;
       });
+
+  void refresh() {
+    setPage(1);
+    fetch();
+  }
+
+  void loadMore() {
+    setPage(state.page + 1);
+    fetch(loadMore: true);
+  }
+
+  void setQuery(String? value) async {
+    if (state.query == value) {
+      return;
+    }
+
+    state = state.copyWith(query: value);
+    fetch();
+  }
+
+  void setPage(int page) {
+    state = state.copyWith(page: page);
+  }
 }
