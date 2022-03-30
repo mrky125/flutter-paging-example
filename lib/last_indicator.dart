1476d235d2437f381f3b50a2726be4da09b88276
mrky125 <4ex21ia@gmail.com>
2022-03-30 17:53:03 +0900

    add indicator

---
+++ lib/last_indicator.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:visibility_detector/visibility_detector.dart';
+
+class LastIndicator extends StatelessWidget {
+  const LastIndicator(this.onVisible, {Key? key}) : super(key: key);
+
+  final VoidCallback onVisible;
+
+  @override
+  Widget build(BuildContext context) {
+    return VisibilityDetector(
+      key: const Key('for detect visibility'),
+      onVisibilityChanged: (info) {
+        if (info.visibleFraction > 0.9) {
+          onVisible();
+        }
+      },
+      child: const Center(
+        child: Padding(
+          padding: EdgeInsets.only(top: 12, bottom: 12),
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
